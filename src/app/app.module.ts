356869595645b6b986a0f3500793bae887d8822f
Danielle.Leblanc-Cyr <danielleleb12@gmail.com>
2018-03-21 21:28:16 -0100

    created gnome-card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { GnomeCardComponent } from './components/gnome-card/gnome-card.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GnomeCardComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/gnome-card/gnome-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gnome-card',
+  templateUrl: './gnome-card.component.html',
+  styleUrls: ['./gnome-card.component.css']
+})
+export class GnomeCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

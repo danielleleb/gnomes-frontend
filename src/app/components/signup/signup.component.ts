23a06ef2f6b5886c364786eefa20687573b243be
Danielle.Leblanc-Cyr <danielleleb12@gmail.com>
2018-03-22 03:01:46 -0100

    added form to signup component and method to signup ts

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  @Output() submitForm = new EventEmitter<any>();
+
+  @Input() feedbackEnabled: boolean;
+  @Input() error: string;
+  @Input() processing: boolean;
+
+  username: string;
+  password: string;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  submitSignUpForm(form) {
+    this.error = null;
+    this.feedbackEnabled = true;
+    if (form.valid) {
+      // this.processing = false;
+      const data = {
+        username: this.username,
+        password: this.password
+      };
+      this.submitForm.emit(data);
+     }
+  }
 }

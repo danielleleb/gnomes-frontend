342530bc55e3720605cc88dca06dbf012c5f7a00
Danielle.Leblanc-Cyr <danielleleb12@gmail.com>
2018-03-22 02:59:53 -0100

    added signup component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { GnomeCardComponent } from './components/gnome-card/gnome-card.component
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { LoginComponent } from './components/login/login.component';
     GnomeCardComponent,
     HomePageComponent,
     LandingPageComponent,
-    LoginComponent
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule

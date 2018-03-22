841fbbafa294d15efed480e4690bf9b916895853
Danielle.Leblanc-Cyr <danielleleb12@gmail.com>
2018-03-22 02:49:17 -0100

    working on gnome-card css, added landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { GnomeCardComponent } from './components/gnome-card/gnome-card.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     GnomeCardComponent,
-    HomePageComponent
+    HomePageComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule

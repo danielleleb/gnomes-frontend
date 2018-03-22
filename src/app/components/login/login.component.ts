5ea7b424968210c6bb8ad7097e13bc4d658117e7
Danielle.Leblanc-Cyr <danielleleb12@gmail.com>
2018-03-22 02:59:23 -0100

    added form to login component and methods in login ts

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  @Output() submitForm = new EventEmitter<any>();
+
+  @Input() feedbackEnabled: boolean;
+  @Input() error: string;
+  @Input() processing: boolean;
+
+  username: String;
+  password: String;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  submitLoginForm(form) {
+    this.error = '';
+    this.feedbackEnabled = true;
+    if (form.valid) {
+      this.processing = false;
+      const data = {
+        username: this.username,
+        password: this.password
+      };
+      this.submitForm.emit(data);
+    }
   }
 
 }
